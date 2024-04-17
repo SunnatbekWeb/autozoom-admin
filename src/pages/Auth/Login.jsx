d3ff9f0a648319c958ac7639ea9cfb7fe2862aa0
Mahmud Toshtemirov <mahmuddev1908@gmail.com>
2024-04-17 16:50:19 +0500

    toastify

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import styles from "./Login.module.css";
 import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Login() {
   const [data, setData] = useState({ phone_number: "", password: "" });
@@ -28,34 +30,38 @@ function Login() {
         "access_token",
         responseData.data?.tokens?.accessToken?.token
       );
+      toast.success("Ma'lumotlar muvaffaqiyatli yuborildi");
       navigate("/");
     } catch (error) {
+      toast.error("Malumotingiz yuborishda hatolik yuz berdi!");
       setError(error.message);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit} className={styles["form-container"]}>
-      <input
-        type="text"
-        name="phoneNumber"
-        value={data.phone_number}
-        onChange={(e) => setData({ ...data, phone_number: e.target.value })}
-        className={styles["input-field"]}
-        placeholder="Phone Number"
-      />
-      <input
-        type="password"
-        name="password"
-        value={data.password}
-        onChange={(e) => setData({ ...data, password: e.target.value })}
-        className={styles["input-field"]}
-        placeholder="Password"
-      />
-      <button type="submit" className={styles["submit-btn"]}>
-        Submit
-      </button>
-    </form>
+    <div className={styles["container"]}>
+      <form onSubmit={handleSubmit} className={styles["form-container"]}>
+        <input
+          type="text"
+          name="phoneNumber"
+          value={data.phone_number}
+          onChange={(e) => setData({ ...data, phone_number: e.target.value })}
+          className={styles["input-field"]}
+          placeholder="Phone Number"
+        />
+        <input
+          type="password"
+          name="password"
+          value={data.password}
+          onChange={(e) => setData({ ...data, password: e.target.value })}
+          className={styles["input-field"]}
+          placeholder="Password"
+        />
+        <button type="submit" className={styles["submit-btn"]}>
+          Submit
+        </button>
+      </form>
+    </div>
   );
 }
 
