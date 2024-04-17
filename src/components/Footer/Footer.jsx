e420285b9318099460c749a094a68337363e9419
sunnatbek07 <sunnatbekqadamov9@gmail.com>
2024-04-17 15:53:17 +0500

    created components and pages

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <h2>Copyright Autozoom.uz 2023-2024</h2>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,14 +1,19 @@
-import { Header } from "antd/es/layout/layout";
+import { Footer, Header } from "antd/es/layout/layout";
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Sidebar from "../components/Sidebar/Sidebar";
 
 const Layout = () => {
   return (
     <>
       <Header />
+      <aside>
+        <Sidebar />
+      </aside>
       <main>
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 };
